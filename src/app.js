30c26870ffc60e02c76ef5545e793a6a8b3b185e
fabricio <vigofabricio@gmail.com>
2023-03-17 22:17:35 -0300

    primera practica integradora

--- src/app.js
+++ src/app.js
@@ -1,48 +1,77 @@
 import express from "express";
 import { engine } from "express-handlebars";
 import { Server } from "socket.io";
+import mongoose from "mongoose";
+import path from "path";
 
-import productsRouter from "./routes/product.js";
-import cartsRouter from "./routes/cart.js";
-import viewsRouter from "./routes/viewsRouter.js";
-import ProductManager from "./managers/ProductManager.js";
-import { __dirname } from "./utils/utils.js";
+import productsFSRouter from "./routes/products.router.js";
+import cartsFSRouter from "./routes/carts.router.js";
+import cartsDBRouter from "./routes/cartsDB.router.js";
+import productsDBRouter from "./routes/productsDB.router.js";
+import viewsRouter from "./routes/views.router.js";
+import ProductManagerFS from "./dao/productManagerFS.js";
+import { __dirname } from "./helpers/utils.js";
+import { messagesModel } from "./dao/models/messages.model.js";
 
 const app = express();
 const port = 8080;
-const pm = new ProductManager(`${__dirname}/files/products.json`);
+const pm = new ProductManagerFS(path.join(__dirname, "../files/products.json"));
 
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
-app.set("views", `${__dirname}/views`);
+app.set("views", path.join(__dirname,"../views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use("/", viewsRouter);
-app.use("/api/products", productsRouter);
-app.use("/api/carts", cartsRouter);
+app.use("/api/productsFS", productsFSRouter);
+app.use("/api/cartsFS", cartsFSRouter);
+app.use("/api/cartsDB", cartsDBRouter);
+app.use("/api/productsDB", productsDBRouter);
 
 const httpServer = app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
+
 
 const io = new Server(httpServer);
 
 io.on("connection", async (socket) => {
   console.log("New client connected");
 
-  let products = await pm.getProducts();
-  socket.emit("products", products);
-
   socket.on("deleteProduct", async (id) => {
     let response = await pm.deleteProductSocket(id);
     socket.emit("deleteProductRes", response);
+    if (response.success) {
+      socket.broadcast.emit("productListUpdated");
+    }
   });
-
+  
   socket.on("addProduct", async (product) => {
     let response = await pm.addProductSocket(product);
     socket.emit("addProductRes", response);
+    if (response.success) {
+      socket.broadcast.emit("productListUpdated");
+    }
   });
+
+  socket.on("newMessage", async ({ user, message }) => {
+    await messagesModel.create({ user: user, message: message });
+    io.emit("messagesListUpdated");
+  })
 });
+
+const connect = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://coderhouse:coderhouse@cluster0.cz0fbqs.mongodb.net/?retryWrites=true&w=majority');
+    console.log("DB connection success");
+  } catch (error) {
+    console.log(`DB connection fail. Error: ${error}`);
+  }
+}
+
+connect();
+
+io.on("error", (error) => console.error(error));
---
+++ src/dao/models/products.model.js
@@ -0,0 +1,38 @@
+import { Schema, model } from "mongoose";
+
+const productsCollection = "products";
+
+const productsSchema = new Schema({
+  code: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: Boolean,
+    default: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  thumbnails: [String],
+});
+
+export const productsModel = model(productsCollection, productsSchema);
